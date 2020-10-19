3bb52909b5df4cf23fb991443979a7b720c53ecd
Stephen <statco19@gmail.com>
2020-10-19 18:26:52 +0900

    10/19

---
+++ demo/scratch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int data = 5;
+    int result = 0;
+
+    if(data > 3);
+    {
+        data++;
+        result = data;
+    }
+
+    printf("data: %d, result: %d\n", data, result);
+
+    return 0;
+}
